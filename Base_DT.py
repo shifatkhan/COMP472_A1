87666d336d166439edf6cbfac0b1a502854a2c4f
shifatkhan <shifatkhan6@gmail.com>
2020-10-17 20:38:03 -0400

    Updated output. TODO: move to util

--- Base_DT.py
+++ Base_DT.py
@@ -2,9 +2,13 @@
 
 from sklearn import tree
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
-import csv
 from pathlib import Path
+import pandas as pd
+import csv
 import util
+
+import warnings
+warnings.filterwarnings('ignore') #Make output cleaner
 
 filename = "./output/Base-DT-DS1.csv"
 
@@ -22,23 +26,48 @@ Y_pred = clf.predict(X_test)
 print(f"Test Accuracy: {metrics.accuracy_score(Y_test, Y_pred)}");
 
 print("\na)")
-Path("./output").mkdir(parents=True, exist_ok=True)
 
-# newline="" because there are empty lines between rows.
-with open(filename, mode='w', newline="") as csv_file:
-    csv_writer = csv.writer(csv_file)
-    
-    for i in range(len(Y_pred)):
-        csv_writer.writerow([i+1, Y_pred[i]])
-        print(f"{i+1}, {Y_pred[i]}")
+for i in range(len(Y_pred)):
+    print(f"{i}, {Y_pred[i]}")
         
 print("\nb)")
 print("Confusion Matrix")
 confusion_matrix = metrics.confusion_matrix(Y_test, Y_pred)
 metrics.plot_confusion_matrix(clf, X_test, Y_test)
+print(confusion_matrix)
 
 print("\nc) & d)")
+classification_report = metrics.classification_report(Y_test, Y_pred)
 print("Classification report for classifier %s:\n%s\n"
-      % (clf, metrics.classification_report(Y_test, Y_pred)))
+      % (clf, classification_report))
 
 
+# OUTPUT
+try: 
+    Path("./output").mkdir(parents=True, exist_ok=True)
+    # newline="" because there are empty lines between rows.
+    with open(filename, mode='w', newline="") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        
+        # Write part A
+        csv_writer.writerow([""]) # newline
+        csv_writer.writerow(["a) Predicted classes"]) # Must be an Iterator, so put []
+        
+        for i in range(len(Y_pred)):
+            csv_writer.writerow([i+1, Y_pred[i]])
+            
+        # Write part B
+        csv_writer.writerow([""]) # newline
+        csv_writer.writerow(["b) Confusion Matrix"])
+        for i in range(len(confusion_matrix)):
+            csv_writer.writerow(confusion_matrix[i])
+            
+        # Write part C & D
+        csv_writer.writerow([""]) # newline
+        csv_writer.writerow(["c) & d) Classification report for classifier"])
+            
+except FileNotFoundError:
+        print(f"File not found: {filename}")
+        
+data_frame = pd.DataFrame(metrics.classification_report(Y_test, Y_pred, output_dict=True)).transpose()
+data_frame.to_csv(filename, mode="a")
