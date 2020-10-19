d79a679011872c5d38b3adabbf9dea73485ce8a8
Akisukei <justin321261@hotmail.com>
2020-10-18 20:35:46 -0400

    done base-mlp

--- Base_MLP.py
+++ Base_MLP.py
@@ -44,13 +44,15 @@ def plot_confusion_matrix(model, y_target, y_predict):
     c_matrix = confusion_matrix(y_target, y_predict)
     print(f'{c_matrix}')
     
+    return c_matrix
+    
     
 def print_model_details(y_target, y_predict):
     report = classification_report(y_target, y_predict)
     print(f'\n{report}\n')
+    
 
-
-def perform_baseMLP():
+def run():
     
     print("Performing Base MLP")
     print("-------------------")
@@ -60,19 +62,29 @@ def perform_baseMLP():
     mlp_model1 = mlp_clfr.fit(train1_x, train1_y)
     mlp_model2 = mlp_clfr.fit(train2_x, train2_y)
     
+    # Predict trained model with dataset
+    y_predict1 = mlp_model1.predict(test1_x)
+    y_predict2 = mlp_model2.predict(test2_x)
+    
     # Evaluate score on dataset
     eval_dataset(1, mlp_model1)
+    
     # Plot confusion matrix
-    test1_y_predict = mlp_model1.predict(test1_x)
-    plot_confusion_matrix(mlp_model1, test1_y, test1_y_predict)
+    c_matrix1 = plot_confusion_matrix(mlp_model1, test1_y, y_predict1)
+    
     # Print precision, recall, f1-score, accuracy, macro-avg f1, weighted-avg f1
-    print_model_details(test1_y, test1_y_predict)
+    print_model_details(test1_y, y_predict1)
     
     # Repeat steps for dataset 2
     eval_dataset(2, mlp_model2)
     test2_y_predict = mlp_model2.predict(test2_x)
-    plot_confusion_matrix(mlp_model2, test2_y, test2_y_predict)
-    print_model_details(test2_y, test2_y_predict)
+    c_matrix2 = plot_confusion_matrix(mlp_model2, test2_y, y_predict2)
+    print_model_details(test2_y, y_predict2)
+    
+    
+    # Output results into file
+    util.write_csv("./output/Base-MLP-DS1.csv", test1_y, y_predict1, c_matrix1)
+    util.write_csv("./output/Base-MLP-DS2.csv", test2_y, y_predict2, c_matrix2)
     
     
     
